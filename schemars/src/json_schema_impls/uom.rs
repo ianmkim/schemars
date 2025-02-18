c92329b727b019008ae9981fd024fc4c73971d9a
Ian Kim <ian.kim@neuralink.com>
2025-02-18 12:59:19 -0600

    changed to fixed

--- schemars/src/json_schema_impls/uom.rs
+++ schemars/src/json_schema_impls/uom.rs
@@ -4,4 +4,4 @@ use uom::si::Quantity;
 
 // This implementation works for any uom::Quantity
 // where V (the storage type) implements JsonSchema.
-forward_impl!((<D: uom::si::Dimension, U: uom::si::Units<V>, V: JsonSchema + uom::Conversion<V> + uom::num_traits::Num> JsonSchema for Quantity<D, U, V>) => V);
+forward_impl!((<D: uom::si::Dimension + ?Sized, U: uom::si::Units<V> + ?Sized, V: JsonSchema + uom::Conversion<V> + uom::num_traits::Num + Sized> JsonSchema for Quantity<D, U, V>) => V);
